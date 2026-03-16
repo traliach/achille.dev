d3e360bffe8910603028a01557518845abe9a798
traliach <ali.achille.traore@gmail.com>
2026-03-16 11:16:17 -0400

    add atlas project ip access list

--- infra/main.tf
+++ infra/main.tf
@@ -6,3 +6,8 @@ resource "mongodbatlas_project" "main" {
     prevent_destroy = true
   }
 }
+
+resource "mongodbatlas_project_ip_access_list" "main" {
+  project_id = mongodbatlas_project.main.id
+  cidr_block = var.atlas_ip_access_cidr
+}
